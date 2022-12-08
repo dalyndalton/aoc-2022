01bec5c5085afd1387efbba7409a34f0ea2d3995
Dalyn Dalton <dalyn.dalton.dd@gmail.com>
2022-12-07 22:38:51 -0700

    refactor for clarity

--- day8/day8.py
+++ day8/day8.py
@@ -11,33 +11,29 @@ with open("input.txt") as file:
                 or all(grid[i][x] < tree for i in range(0, y))
                 or all(grid[i][x] < tree for i in range(y + 1, len(grid)))
             )
-            
-    ans = []
+
+    point_scores = []
     for y, row in enumerate(grid):
         for x, tree in enumerate(row):
-            ans.append([0, 0, 0, 0])
-
+            point_scores.append([0, 0, 0, 0])
             for j in range(x - 1, -1, -1):
-                ans[-1][0] += 1
+                point_scores[-1][0] += 1
                 if grid[y][j] >= tree:
                     break
-
             for i in range(y - 1, -1, -1):
-                ans[-1][1] += 1
+                point_scores[-1][1] += 1
                 if grid[i][x] >= tree:
                     break
-
             for j in range(x + 1, len(row)):
-                ans[-1][2] += 1
+                point_scores[-1][2] += 1
                 if grid[y][j] >= tree:
                     break
-
             for i in range(y + 1, len(grid)):
-                ans[-1][3] += 1
+                point_scores[-1][3] += 1
                 if grid[i][x] >= tree:
                     break
-                
-    sol = max(prod(x) for x in ans)   
-    
+
+    sol = max(prod(score) for score in point_scores)
+
     print(f"Visible Trees: {vis}")
     print(f"Other Thing: {sol}")
